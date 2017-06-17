69faadfc36f42254006bd81dfa4c675c8f267af3
Mark Hjorth Sørensen <MarkHjorthSorensen@icloud.com>
2017-06-17 16:31:32 +0200

    Added Google Structured Data for Person

---
+++ js/person.js
@@ -0,0 +1,21 @@
+<script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "Person",
+  "name": "Mark Hjorth",
+  "givenName": "Mark Hjorth",
+  "familyName": "Sørensen",
+  "image": "http://markhjorth.com/img/profile.png",
+  "url": "http://markhjorth.com",
+  "sameAs": [
+    "http://www.facebook.com/MarkHjorthSorensen",
+    "http://instagram.com/MarkHjorth",
+    "http://www.linkedin.com/in/MarkHjorthSorensen",
+    "http://plus.google.com/+MarkHjorthSorensen",
+    "https://github.com/markhjorth",
+    "https://bitbucket.org/MarkHjorth",
+    "https://twitter.com/markhjorth",
+    "https://reddit.com/user/MarkHjorth"
+  ]
+}
+</script>
